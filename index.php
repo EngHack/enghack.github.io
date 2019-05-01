83a3cdd3ec064e402be262dc8272350eb55f73a0
lee-ma <leema9321@gmail.com>
2019-05-01 11:41:18 -0600

    Add application typeforms and buttons

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<?php include_once("index.html"); ?>
+<?php
+  $url_route = htmlspecialchars($_SERVER['REQUEST_URI'], ENT_QUOTES, 'UTF-8');
+  if(isset($url_route)) {
+    if($url_route == "/hacker-application") {
+      include_once("hacker-application.html");
+    } else if($url_route == "/mentor-application") {
+      include_once("mentor-application.html");
+    }else {
+      include_once("index.html");
+    }
+  }
+?>
